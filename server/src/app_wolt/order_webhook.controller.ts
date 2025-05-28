88259ce37f522d0aae0d1de8eee75b351dea4ea9
haianhxanh <hanka7n@gmail.com>
2025-05-28 16:54:26 +0200

    Update response status Wolt

--- server/src/app_wolt/order_webhook.controller.ts
+++ server/src/app_wolt/order_webhook.controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
 export const order_webhook = async (req: Request, res: Response) => {
-  const { order } = req.body;
-  console.log(order);
+  console.log(req.body);
   return res.status(200).json({
-    order,
+    status: "ok",
   });
 };
